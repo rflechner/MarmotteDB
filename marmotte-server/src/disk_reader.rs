2fa713b9060e2ec8ffedf584bf1a221369d90cd9
rflechner <rflechner@romcyber.com>
2025-08-29 17:56:52 +0200

    feat: add DiskReader for reading records and integrate it into main.rs

---
+++ marmotte-server/src/disk_reader.rs
@@ -0,0 +1,143 @@
+﻿use std::borrow::Cow;
+use std::cell::Cell;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Seek, SeekFrom};
+use bytes::BytesMut;
+use crate::binary::BinaryReader;
+use crate::disk_writer::{Record, RecordsFileMeta};
+
+pub struct DiskReaderOptions {
+    pub max_record_size: u64
+}
+
+impl DiskReaderOptions {
+
+    pub fn create_default() -> DiskReaderOptions {
+        DiskReaderOptions { max_record_size: 80 * 1024 * 1024 }
+    }
+
+}
+
+pub struct DiskReader {
+    pub file_name: String,
+    pub file: File,
+    pub meta: Cell<RecordsFileMeta>,
+    pub position: u64,
+    pub options: DiskReaderOptions
+}
+
+impl DiskReader {
+
+    pub fn new(file_name: &str, options: DiskReaderOptions) -> DiskReader {
+        let file = OpenOptions::new().read(true).open(file_name).unwrap();
+
+        let mut reader = DiskReader {
+            file_name: String::from(file_name),
+            file,
+            meta: Cell::new(RecordsFileMeta::empty()),
+            position: RecordsFileMeta::size() as u64,
+            options
+        };
+        reader.load_metadata();
+        reader
+    }
+
+    pub fn load_metadata(&mut self) {
+        let m = RecordsFileMeta::read_metadata(&mut self.file);
+        self.meta.set(m);
+    }
+
+    pub fn rewind_to_start(&mut self) {
+        (&self.file).seek(SeekFrom::Start(RecordsFileMeta::size() as u64)).unwrap();
+    }
+
+    pub fn seek_to(&mut self, position: u64) {
+        (&self.file).seek(SeekFrom::Start(position)).unwrap();
+    }
+
+    pub fn read_next_record (&mut self) -> Result<Box<Record>, Cow<'static, str>> {
+        let meta = self.meta.get();
+        let mut len_buf = vec![0; 8];
+        (&self.file).read_exact(&mut len_buf).unwrap();
+        let mut len_bin = BinaryReader::from(BytesMut::from(len_buf.as_slice()));
+        let len = len_bin.read_u64().unwrap();
+
+        let mut hash_buf = vec![0; 4];
+        (&self.file).read_exact(&mut hash_buf).unwrap();
+        let mut hash_bin = BinaryReader::from(BytesMut::from(hash_buf.as_slice()));
+        let hash = hash_bin.read_u32().unwrap();
+
+        if len > self.options.max_record_size {
+            let message = format!("record length is {} bytes. max allowed id {} bytes", len, self.options.max_record_size);
+            Err(Cow::Owned(message))
+        } else {
+            let mut buf: Vec<u8> = vec![0; len as usize];
+            (&self.file).read_exact(&mut buf).unwrap();
+
+            let mut deleted_buf: Vec<u8> = vec![0; 1];
+            (&self.file).read_exact(&mut deleted_buf).unwrap();
+            let deleted = deleted_buf[0] != 0;
+
+            let checksum = crc32fast::hash(&buf);
+
+            if checksum != hash {
+                Err(Cow::Owned("corrupted record".to_owned()))
+            }
+            else {
+                let record = Record { position: meta.position, content_size: len, content: buf.to_vec(), deleted: deleted, checksum: checksum };
+                Ok(Box::new(record))
+            }
+        }
+    }
+
+    pub fn find_record<F> (&mut self, f: F) -> Option<Box<Record>> where F : Fn(Box<Record>, u64) -> bool {
+        self.rewind_to_start();
+
+        let mut current_id = 0;
+        let meta = self.meta.get();
+
+        loop {
+            current_id += 1;
+
+            if self.file.stream_position().unwrap() < meta.position {
+                let res = self.read_next_record();
+                match res {
+                    Err(_) => break None,
+                    Ok(record) => {
+                        if f(record.clone(), current_id) {
+                            break Some(record);
+                        }
+                    }
+                }
+            } else {
+                break None
+            }
+
+        }
+    }
+
+}
+
+impl Iterator for DiskReader {
+
+    type Item = Box<Record>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let meta = self.meta.get();
+
+        loop {
+            if self.file.stream_position().unwrap() < meta.position {
+                let res = self.read_next_record();
+                match res {
+                    Err(_) => break None,
+                    Ok(record) => {
+                        break Some(record);
+                    }
+                }
+            } else {
+                break None
+            }
+        }
+    }
+
+}
--- marmotte-server/src/main.rs
+++ marmotte-server/src/main.rs
@@ -1,11 +1,13 @@
+mod binary;
+mod binary_serializer;
+mod disk_writer;
+mod disk_reader;
+
 use std::time::Instant;
 
 use figlet_rs::FIGfont;
 use crate::disk_writer::DiskWriter;
-
-mod binary;
-mod binary_serializer;
-mod disk_writer;
+use crate::disk_reader::{DiskReader, DiskReaderOptions};
 
 fn main() {
     let font = FIGfont::standard().unwrap();
@@ -14,13 +16,13 @@ fn main() {
     println!("{}", figure.unwrap());
 
 
-    let mut data1 = DiskWriter::new("test1.data", 2048);
-    let mut data2 = DiskWriter::new("test2.data", 2048);
+    let mut data_writer_1 = DiskWriter::new("test1.data", 2048);
+    let mut data_writer_2 = DiskWriter::new("test2.data", 2048);
 
     let mut iter_start = Instant::now();
 
-    let batch_size = 1000;
-    let bench_length = 30_000;
+    let batch_size = 100;
+    let bench_length = 2_000;
 
     println!("Starting to write records in batches of {}...", batch_size);
 
@@ -32,7 +34,7 @@ fn main() {
 
         if i % batch_size == 0 {
             let references: Vec<&[u8]> = batch.iter().map(|v| v.as_slice()).collect();
-            data1.bulk_add_records(references);
+            data_writer_1.bulk_add_records(references);
 
             batch.clear();
 
@@ -48,7 +50,7 @@ fn main() {
 
     for i in 1..bench_length+1 {
 
-        data2.add_record(format!("Record number {}!", i).as_bytes());
+        data_writer_2.add_record(format!("Record number {}!", i).as_bytes());
 
         if i % batch_size == 0 {
             println!("Wrote {} records", i);
@@ -59,5 +61,24 @@ fn main() {
         }
     }
 
-    println!("Done!");
+    println!("Write bench done!");
+
+    println!("Starting to read records...");
+
+    let mut data_reader_1 = DiskReader::new("test1.data", DiskReaderOptions::create_default());
+    let mut data_reader_2 = DiskReader::new("test2.data", DiskReaderOptions::create_default());
+
+    println!("Reading records of test1.data ...");
+    
+    for item in data_reader_1 {
+        let text = String::from_utf8(item.content).unwrap();
+        println!("{}", text);
+    }
+    
+    println!("Reading records of test2.data ...");
+    for item in data_reader_2 {
+        let text = String::from_utf8(item.content).unwrap();
+        println!("{}", text);
+    }
+    
 }
